1759e313fe2c94521b2a3aec2d826ba7b6b63f2e
darias03 <darademanenko932@gmail.com>
2025-12-20 02:45:02 +0300

    ggf

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,21 +2,26 @@
 
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-  case '+': return num1 + num2;
-  case '-': return num1 - num2;
-  case '*': return num1 * num2;
-  default: throw new Error(`Unknown operator: ${operator}`);
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error('Unknown operator');
   }
 };
 
 const generateRound = () => {
   const num1 = getSecureRandomInt(1, 50);
   const num2 = getSecureRandomInt(1, 50);
-  const operator = getSecureRandomInt(0, operators.length - 1);
-  
+  const operatorIndex = getSecureRandomInt(0, operators.length - 1);
+  const operator = operators[operatorIndex];
+
   const question = `${num1} ${operator} ${num2}`;
   const correctAnswer = String(calculate(num1, num2, operator));
-  
+
   return [question, correctAnswer];
 };
 
