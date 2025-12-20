c21c1ef6fcd4870d6722eb59b621fbe737d4d7ef
darias03 <darademanenko932@gmail.com>
2025-12-20 20:42:46 +0300

    chore: remove duplicate test, final cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 export default {
   testMatch: ["**/__tests__/**/*.js"],
   coveragePathIgnorePatterns: [
-    '/bin/',
+    '/bin/',           // CLI-скрипты
+    '/node_modules/',
+    '/coverage/',
+    '/__tests__/',
+    'jest.config.js',
+    'babel.config.js',
   ],
 };
