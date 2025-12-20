4fc99a06e3c819c955f91c39110997d61b64cb08
darias03 <darademanenko932@gmail.com>
2025-12-21 00:57:15 +0300

    ffv

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 export default {
-  testMatch: ["**/__tests__/**/*.js"],
-  coveragePathIgnorePatterns: [
-    '/bin/',           // CLI-скрипты
-    '/node_modules/',
-    '/coverage/',
-    '/__tests__/',
-    'jest.config.js',
-    'babel.config.js',
-  ],
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text'],
 };
